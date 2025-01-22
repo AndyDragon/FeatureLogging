2449f9fa90b752e297619f3413f4c81e5e3aee0c
Andrew Forget <andrew.forget@andydragon.com>
2025-01-21 22:10:26 -0500

    Minor change to screenshot prep script

--- prepare-app-for-screenshot.sh
+++ prepare-app-for-screenshot.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 defaults write com.andydragon.Feature-Logging "NSWindow Frame Feature_Logging.ContentView-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 "
-
 defaults write com.andydragon.Feature-Logging "NSWindow Frame NSNavPanelAutosaveName" "0 0 1280 800 0 0 1280 800 "
-
 defaults write com.andydragon.Feature-Logging "NSWindow Frame SwiftUI.ModifiedContent<Feature_Logging.ContentView, SwiftUI._EnvironmentKeyWritingModifier<Swift.Optional<Feature_Logging.AppCommandModel>>>-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 "
-
 defaults write com.andydragon.Feature-Logging "NSWindow Frame SwiftUI.ModifiedContent<SwiftUI.ModifiedContent<Feature_Logging.ContentView, SwiftUI._EnvironmentKeyWritingModifier<Swift.Optional<Feature_Logging.AppCommandModel>>>, SwiftUI._AppearanceActionModifier>-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 "
-
-##defaults write com.andydragon.Feature-Logging "NSWindow Frame SwiftUI.ModifiedContent<SwiftUI.ModifiedContent<SwiftUI.ModifiedContent<Feature_Logging.ContentView, SwiftUI._EnvironmentKeyWritingModifier<Swift.Optional<Feature_Logging.AppCommandModel>>>, SwiftUI._AppearanceActionModifier>, SwiftUI._AppearanceActionModifier>-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 {\\"tilingState\\":{\\"tilingPosition\\":9,\\"normalizedSize\\":1,\\"untiledFrame\\":\\"{{731, 347}, {1024, 772}}\\"}}"
