5fbbb026114dc70a4732292cf729b15e256ab2fb
Andrew Forget <andrew.forget@andydragon.com>
2025-01-22 23:20:01 -0500

    Adding MAUI app for possible Android app in c#

---
+++ Feature Logging MAUI/Feature Logging MAUI/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Feature_Logging_MAUI
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
